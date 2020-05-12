848c57de57701408c2ba09dd1a309af31b57a5c8
Saarthak Sangamnerkar <31529049+saarthaksangam@users.noreply.github.com>
2020-05-12 20:57:33 +0530

    Create Migratory_Birds.py

---
+++ Implementation/Migratory_Birds.py
@@ -0,0 +1,19 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+from collections import Counter
+
+# Complete the migratoryBirds function below.
+def migratoryBirds(arr):
+    arr.sort()
+    result=Counter(arr)
+    print(max(result, key=result.get))
+
+if __name__ == '__main__':
+    arr_count = int(input().strip())
+    arr = list(map(int, input().rstrip().split()))
+    result = migratoryBirds(arr)
